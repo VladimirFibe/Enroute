c46936311c67e5b87eb48110e00e2c76f7aaa6e6
Vladimir <motiw@icloud.com>
2023-03-17 11:55:10 +0600

    save

--- Enroute/EnrouteApp.swift
+++ Enroute/EnrouteApp.swift
@@ -4,10 +4,15 @@ import SwiftUI
 struct EnrouteApp: App {
     let persistenceController = PersistenceController.shared
 
+    
     var body: some Scene {
         WindowGroup {
             FlightsEnrouteView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .onAppear {
+                    print((Bundle.main.object(forInfoDictionaryKey: "FlightAware Credentials") as? String) ?? "")
+                }
         }
+        
     }
 }
